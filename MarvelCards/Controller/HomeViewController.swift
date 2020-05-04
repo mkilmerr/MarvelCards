72b660a3f28debc0ae9d4ff3cc512f9ec899da8a
mkilmer <marcoskilmer1@gmail.com>
2020-05-04 17:16:27 -0300

    Add Launch Screen

---
+++ MarvelCards/Controller/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  MarvelCards
+//
+//  Created by Marcos Kilmer on 04/05/20.
+//  Copyright © 2020 mkilmer. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        self.view.backgroundColor = .white
+    }
+}
